8c4aba8652ad0d9dbc0ac4c88acc70ed69c97f9f
Afirez <707627402@qq.com>
2020-06-03 23:13:25 +0800

    basiclly try something

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,25 @@
-#![deny(warnings)]
+use socket_chat::ThreadPool;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
-use mio::net::TcpListener;
-use mio::*;
-use net2::unix::UnixTcpBuilderExt;
-use net2::TcpBuilder;
-use slab::Slab;
-use std::env;
-use std::io;
-use std::io::ErrorKind;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(10);
 
-mod buf;
-mod client;
-mod server;
-mod util;
-
-const SERVER: Token = Token(0);
-
-fn main() -> io::Result<()> {
-    let mut addr = "0.0.0.0:".to_owned();
-    if let Some(port) = env::args().nth(1) {
-        addr.push_str(&port);
-    } else {
-        addr.push_str("9999");
+    for stream in listener.incoming().take(100000) {
+        let stream = stream.unwrap();
+        pool.execute(handle_connection(stream));
     }
 
-    let sock = TcpBuilder::new_v4()?;
+    println!("Shutting down.");
+}
 
-    if cfg!(unix) {
-        sock.reuse_address(true)?;
-        sock.reuse_port(true)?;
-    }
-
-    sock.bind(addr)?;
-    let listener = sock.listen(1024)?;
-    let listener = TcpListener::from_std(listener)?;
-    // let listener = TcpListener::bind(addr)?;
-
-    let mut poll = Poll::new()?;
-    let mut slab = Slab::new();
-    let mut events = Events::with_capacity(1024);
-
-    poll.registry()
-        .register(&listener, SERVER, Interests::READABLE)?;
-
-    loop {
-        poll.poll(&mut events, None)?;
-        for event in events.iter() {
-            match event.token() {
-                SERVER => {
-                    server::accept(&listener, &mut slab, poll.registry())?;
-                }
-                Token(t) => {
-                    let k = util::token_to_key(t);
-                    if let Some(c) = slab.get_mut(k) {
-                        if let Err(e) = c.handle(t, event, poll.registry()) {
-                            slab.remove(k);
-                            if e.kind() != ErrorKind::Other {
-                                println!("ERR: {:?}", e);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    stream.write(&buffer).unwrap();
 }
