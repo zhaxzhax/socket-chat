786c0b2c11f1dd49505112ad193626f509c346b0
Afirez <707627402@qq.com>
2020-06-06 12:26:58 +0800

    finish demo

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,40 +1,30 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow,ipcMain} = require('electron')
 const path = require('path')
 var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 7878;
 
 var client = new net.Socket();
-function ()
 client.connect(PORT, HOST, function() {
 
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // 建立连接后立即向服务器发送数据，服务器将收到这些数据
-    client.write('Hello!');
+    client.write('~');
 
 });
-
+setInterval(function(){client.write('~');},500)
 // 为客户端添加“data”事件处理函数
 // data是服务器发回的数据
-client.on('data', function(data) {
 
-    console.log('DATA: ' + data);
-    // 完全关闭连接
-    client.destroy();
-});
-
-// 为客户端添加“close”事件处理函数
-client.on('close', function() {
-    console.log('Connection closed');
-});
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+	  nodeIntegration:true
     }
   })
 
@@ -43,6 +33,17 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  client.on('data', function(data) {
+  
+      console.log('DATA: ' + data);
+      // 完全关闭连接
+      mainWindow.webContents.send('getMsg', data);
+  });
+  
+  // 为客户端添加“close”事件处理函数
+  client.on('close', function() {
+      console.log('Connection closed');
+  });
 }
 
 // This method will be called when Electron has finished
@@ -54,10 +55,13 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	
   })
 })
-
+ipcMain.on('show-window', (event,arg) => {
+	  client.write(arg);
+	 });
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
